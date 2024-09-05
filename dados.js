701d8da144db2b0002e190d7e330f7abbbc91845
Darly <darly_amano123@hotmail.com>
2024-09-05 18:04:16 -0300

    primeiro commit

---
+++ dados.js
@@ -0,0 +1,13 @@
+let dados = [{titulo : 'The Bear',
+    descricao : '"O Urso" é uma série dramática que nos leva para a frenética rotina de uma cozinha de sanduíches em Chicago. Após a trágica morte de seu irmão, Carmy, um renomado chef de alta gastronomia, retorna à sua cidade natal para assumir a lanchonete familiar.',
+    link : 'https://www.disneyplus.com/pt-br/series/the-bear/52m6nx7HoP5F'},
+    
+    {titulo: 'A Casa do Dragão',
+    descricao: 'Uma prequela de Game of Thrones, "A Casa do Dragão" narra a história da Casa Targaryen e a guerra civil que eclodiu por causa da sucessão ao Trono de Ferro, conhecida como a Dança dos Dragões.',
+    link: 'https://www.hbomax.com/pt-br/series/a-casa-do-dragao'},
+
+    { titulo : 'Dark Matter',
+    descricao : 'Uma equipe amnésica acorda a bordo de uma nave espacial, sem memória de quem são ou como chegaram lá. Juntos, eles devem desvendar os mistérios da nave e de suas próprias identidades.',
+    link : 'https://tv.apple.com/br/episode/are-you-happy-in-your-life/umc.cmc.59p6kv4nynpisd1gl300a7810'
+}];
+
